403be7bc93ef1f095785660b98b172dd4a4de71f
Cen Wang <cenwang@umass.edu>
2023-07-27 21:14:29 -0400

    Add automatic Gurobi version detection

--- grb-sys2/build.rs
+++ grb-sys2/build.rs
@@ -1,13 +1,16 @@
 use std::env;
 use std::fmt::{self, Display};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
-const LIB_NAME: &'static str = "gurobi95";
+use regex::Regex;
 
 #[derive(Debug)]
 enum Error {
   DoesNotExist(PathBuf),
   GurobiHomeNotGiven,
+  GurobiClNotFound,
+  CannotParseGurobiVersion,
 }
 
 impl Display for Error {
@@ -19,13 +22,15 @@ impl Display for Error {
         p
       )),
       Error::GurobiHomeNotGiven => f.write_str("GUROBI_HOME not set"),
+      Error::GurobiClNotFound => f.write_str("gurobi_cl not found"),
+      Error::CannotParseGurobiVersion => f.write_str("Cannot get Gurobi version"),
     }
   }
 }
 
 impl std::error::Error for Error {}
 
-fn try_gurobi_home() -> Result<PathBuf, Error> {
+fn get_gurobi_home() -> Result<PathBuf, Error> {
   let path = env::var("GUROBI_HOME").map_err(|_| Error::GurobiHomeNotGiven)?;
 
   // You cannot unset environment variables in the config.toml so this is the next best thing.
@@ -33,22 +38,51 @@ fn try_gurobi_home() -> Result<PathBuf, Error> {
     return Err(Error::GurobiHomeNotGiven);
   }
 
-  let mut path: PathBuf = path.into();
-
-  if cfg!(target_os = "windows") {
-    path.push("bin");
-  } else {
-    path.push("lib");
-  }
+  let path: PathBuf = path.into();
 
   path.canonicalize().map_err(|_| Error::DoesNotExist(path))
 }
 
-fn main() {
-  match try_gurobi_home() {
-    Ok(path) => println!("cargo:rustc-link-search=native={}", path.display()),
-    Err(Error::GurobiHomeNotGiven) => eprintln!("GUROBI_HOME env var not set"),
-    Err(e) => println!("cargo:warning={}", e),
+fn get_gurobi_library(gurobi_home: &Path) -> Result<String, Error> {
+  let gurobi_cl = gurobi_home.join("bin").join("gurobi_cl");
+
+  if !gurobi_cl.exists() {
+    return Err(Error::GurobiClNotFound);
   }
-  println!("cargo:rustc-link-lib=dylib={}", LIB_NAME);
+
+  let output = Command::new(gurobi_cl)
+    .arg("--version")
+    .output()
+    .map_err(|_| Error::CannotParseGurobiVersion)?
+    .stdout;
+  let output = String::from_utf8(output).map_err(|_| Error::CannotParseGurobiVersion)?;
+
+  let re = Regex::new(r"Gurobi Optimizer version (\d+).(\d+).(\d+)").unwrap();
+  let captures = re.captures(&output).unwrap();
+
+  let major = &captures[1];
+  let minor = &captures[2];
+
+  Ok(format!("gurobi{}{}", major, minor))
+}
+
+fn try_main() -> Result<(), Error> {
+  let gurobi_home = get_gurobi_home()?;
+
+  let library = get_gurobi_library(&gurobi_home)?;
+
+  println!(
+    "cargo:rustc-link-search=native={}",
+    gurobi_home.join("lib").display()
+  );
+  println!("cargo:rustc-link-lib=dylib={}", library);
+
+  Ok(())
+}
+
+fn main() {
+  if let Err(e) = try_main() {
+    eprintln!("{}", e);
+    std::process::exit(1);
+  }
 }
